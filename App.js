2c87622280b09894a40bcce48a528dd4dfd71798
Splendid_13 <ikeoladarey@gmail.com>
2021-11-04 21:10:30 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import axios from 'axios'
 
-export default function App() {
+export default function App () {
+  let [quote, setQuote] = React.useState('')
+  let [source, setSource] = React.useState('')
+
+  const fetchApiCall = () => {
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+        'x-rapidapi-key': '8d6e2c9909msh7adbb12c9ed4274p1d0e4ajsn8d9f2fedec8c'
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        setQuote(response.content)
+        setSource(response.originator.name)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const axiosApiCall = () => {
+    axios({
+      method: 'GET',
+      url: 'https://quotes15.p.rapidapi.com/quotes/random/',
+      headers: {
+        'content-type': 'application/octet-stream',
+        'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+        'x-rapidapi-key': '8d6e2c9909msh7adbb12c9ed4274p1d0e4ajsn8d9f2fedec8c',
+        useQueryString: true
+      },
+      params: {
+        language_code: 'en'
+      }
+    })
+      .then(response => {
+        setQuote(response.data.content)
+        setSource(response.data.originator.name)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Making Api call with Fetch and Axios</Text>
+      <TouchableHighlight onPress={fetchApiCall}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Use Fetch API</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={axiosApiCall}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Use Axios</Text>
+        </View>
+      </TouchableHighlight>
+      <View>
+        <Text style={styles.quote}>{quote}</Text>
+        <Text style={styles.source}>{source}</Text>
+      </View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +75,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  button: {
+    padding: 10,
+    marginVertical: 15,
+    backgroundColor: '#0645AD'
+  },
+  buttonText: {
+    color: '#fff'
+  },
+  quote: {
+    fontSize: 17,
+    textAlign: 'center',
+    fontStyle: 'italic'
+  },
+  source: {
+    textAlign: 'right',
+    marginTop: 15
+  },
+  quoteContainer: {
+    marginHorizontal: 20,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5
+  }
+})
